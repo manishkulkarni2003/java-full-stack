6a18e4424a299227f29c87a30afe07a55ef7cbf3
manishkulkarni2003 <165151263+manishkulkarni2003@users.noreply.github.com>
2024-05-16 16:24:37 +0530

    Update usage.java

--- usage.java
+++ usage.java
@@ -2,19 +2,19 @@ package employee;
 
 
     public static void main(String[] args) {
-        // Create an Employee instance
+       
         Employee emp = new Employee("Manish", "E1234", 50000);
 
-        // Accessing attributes using getter methods
+       
         System.out.println("Name: " + emp.getName());
         System.out.println("Employee ID: " + emp.getEmployeeId());
         System.out.println("Salary: " + emp.getSalary());
 
-        // Modifying attributes using setter methods
+        
         emp.setName("Swapnil");
         emp.setSalary(60000);
 
-        // Display modified attributes
+    
         System.out.println("\nAfter modification:");
         System.out.println("Name: " + emp.getName());
         System.out.println("Salary: " + emp.getSalary());
@@ -23,20 +23,20 @@ package employee;
 
 
  public static void main(String[] args) {
-        // Create a Developer instance
+        
         Developer developer = new Developer("Jane Doe", "D456", 60000, "Java");
 
-        // Accessing attributes using getter methods
+       
         System.out.println("Name: " + developer.getName());
         System.out.println("Employee ID: " + developer.getEmployeeId());
         System.out.println("Salary: " + developer.getSalary());
         System.out.println("Programming Language: " + developer.getProgrammingLanguage());
 
-        // Modifying attributes using setter methods
+   
         developer.setSalary(70000);
         developer.setProgrammingLanguage("Python");
 
-        // Display modified attributes
+      
         System.out.println("\nAfter modification:");
         System.out.println("Salary: " + developer.getSalary());
         System.out.println("Programming Language: " + developer.getProgrammingLanguage());
